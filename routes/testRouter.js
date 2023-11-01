c73e7091381d2c4ae13d7957aea6b6e938db2dcd
김수민 <rtn2001@naver.com>
2023-11-01 22:59:15 +0900

    Add linux command(ls) test

--- routes/testRouter.js
+++ routes/testRouter.js
@@ -7,13 +7,12 @@ router.get('/', (req, res) => {
   res.render("index");
 });
 
-router.post("/command", (req, res) => {
+router.post("/command-who", (req, res) => {
   // requset 에서 json 정보 가져오기
   const body = req.body;
   const command = body.command;
-
+  
   // command 실행
-
   childProcess.exec(command, (error, stdout, stderr) => {
     if (error) {
       console.error(`error: ${error}`);
@@ -23,6 +22,25 @@ router.post("/command", (req, res) => {
       console.log(`command result: ${stdout}`);
       res.json({ result: stdout });
     }
+  });
 });
-})
+
+router.post("/command-ls", (req, res) => {
+  // requset 에서 json 정보 가져오기
+  const body = req.body;
+  const command = body.command;
+  
+  // command 실행
+  childProcess.exec(command, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`error: ${error}`);
+      console.error(`stderr: ${stderr}`);
+      res.status(500).json({ error: '명령 실행 중 에러 발생' });
+    } else {
+      console.log(`command result: ${stdout}`);
+      res.json({ result: stdout });
+    }
+  });
+});
+
 module.exports = router;
--- src/CmdTest.js
+++ src/CmdTest.js
@@ -1,10 +1,9 @@
-const $btn = document.querySelector("#whoBtn");
-console.log(document);
-console.log($btn);
+const $whoBtn = document.querySelector("#whoBtn");
+const $lsBtn = document.querySelector("#lsBtn");
 
-$btn.addEventListener("click", (e) => {
+$whoBtn.addEventListener("click", (e) => {
   // command 실행 요청
-  fetch("/test/command", {
+  fetch("/test/command-who", {
   method: "POST",
   headers: {"Content-Type": "application/json"},
   body: JSON.stringify({
@@ -16,6 +15,21 @@ $btn.addEventListener("click", (e) => {
     const who = data.result.split(" ")[0];
     const $input = document.querySelector("#whoInput");
     $input.value= who;
+  }).catch((err) => console.error(new Error(err)))
+});
 
+$lsBtn.addEventListener("click", (e) => {
+  // command 실행 요청
+  fetch("/test/command-ls", {
+  method: "POST",
+  headers: {"Content-Type": "application/json"},
+  body: JSON.stringify({
+    "command": "ls"
+  }),
+}).then((response) => response.json()) // 요청 후 받음
+  .then((data) => {
+    const ls = data.result.split("\n");
+    const $input = document.querySelector("#lsInput");
+    $input.value = ls.map(e => `${e}\n`).join("");
   }).catch((err) => console.error(new Error(err)))
 });
